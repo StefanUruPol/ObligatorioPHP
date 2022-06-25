ea3401fc63a7d28d04b3c8967c9e115c1066b405
tomasdeleon <deleontomas99@gmail.com>
2022-06-25 16:03:44 -0300

    update

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <html>
-  <head>
-    <title>Banco PHP</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  </head>
-  <body>
+
+<head>
+  <title>Banco PHP</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body align="center">
+  <header style=" padding: 3px;background-color: #001a57;"><br/>
+    <h1><img src="WampServer-logo.png" width='90px' height='90px' /> <font color="#FFFFFF">Banco PHP</font></h1><br/>
+  </header></br>
+  <fieldset style=" width:450px; margin:auto;"></br>
     <form>
-      <h1>Banco PHP</h1>
 
-      Elija usuario:<br/><br/>
+      <h2>Elija el tipo de usuario:</h2><br/>
 
-      <button type="button" name="empresa"><a href= "/ObligatorioPHP/empresa/login.php"> Empresa </a></button>
-      <button type="button" name="persona"><a href= "/ObligatorioPHP/persona/login.php"> Persona </a></button>
+      <button type="button" name="persona"><a href="/ObligatorioPHP/persona/login.php"> Persona </a></button>
+      <button type="button" name="empresa"><a href="/ObligatorioPHP/empresa/login.php"> Empresa </a></button>
 
     </form>
-  </body>
+  </fieldset>
+</body>
+
 </html>
