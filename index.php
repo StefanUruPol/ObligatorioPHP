a719504d1be48171a6539392811ef376d0f3045c
tomasdeleon <deleontomas99@gmail.com>
2022-06-25 18:31:57 -0300

    update

--- index.php
+++ index.php
@@ -7,13 +7,15 @@
 </head>
 
 <body align="center">
-  <header style=" padding: 3px;background-color: #001a57;"><br/>
-    <h1><img src="WampServer-logo.png" width='90px' height='90px' /> <font color="#FFFFFF">Banco PHP</font></h1><br/>
+  <header style=" padding: 3px;background-color: #001a57;"><br />
+    <h1><img src="WampServer-logo.png" width='90px' height='90px' />
+      <font color="#FFFFFF">Banco PHP</font>
+    </h1><br />
   </header></br>
   <fieldset style=" width:450px; margin:auto;"></br>
     <form>
 
-      <h2>Elija el tipo de usuario:</h2><br/>
+      <h2>Elija el tipo de usuario:</h2><br />
 
       <button type="button" name="persona"><a href="/ObligatorioPHP/persona/login.php"> Persona </a></button>
       <button type="button" name="empresa"><a href="/ObligatorioPHP/empresa/login.php"> Empresa </a></button>
