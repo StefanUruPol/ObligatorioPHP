a8e03682dfe8ae53cb81c7b659e85e9061b72b34
StefanUruPol <98224250+StefanUruPol@users.noreply.github.com>
2022-07-12 23:49:06 -0300

    Update credencial.php

--- Clases/credencial.php
+++ Clases/credencial.php
@@ -6,13 +6,18 @@
         private $fecha_valida_hasta;
         private $pin;
         private $fecha_y_hora;
+        private $CI_persona;
+        private $RUT_empresa;
         
-        public function __construct($tipo, $codigo, $fecha_valida_desde, $fecha_valida_hasta, $pin, $fecha_y_hora) {
+        public function __construct($tipo, $codigo, $fecha_valida_desde, $fecha_valida_hasta, 
+        $pin, $fecha_y_hora, $CI_persona, $RUT_empresa) {
             $this-> tipo = $tipo;
             $this-> codigo = $codigo;
             $this-> fecha_valida_desde = $fecha_valida_desde;
             $this-> fecha_valida_hasta = $fecha_valida_hasta;
             $this-> pin = $pin;
             $this-> fecha_y_hora = $fecha_y_hora;
+            $this-> CI_persona = $CI_persona;
+            $this-> RUT_empresa = $RUT_empresa;
         }
     }
