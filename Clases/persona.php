6c5841539386a7d8d1a856bcdb010327f3acc89e
StefanUruPol <98224250+StefanUruPol@users.noreply.github.com>
2022-07-12 23:51:24 -0300

    Update persona.php

--- Clases/persona.php
+++ Clases/persona.php
@@ -8,4 +8,18 @@
         private $fecha_nacimiento;
         private $foto;
         private $codigo_credencial;
+        
+        
+        public function __construct($ci, $primer_nombre, $segundo_nombre, $primer_apellido, 
+        $segundo_apellido, $fecha_nacimiento, $foto, $codigo_credencial) {
+            $this-> ci = $ci;
+            $this-> primer_nombre = $primer_nombre;
+            $this-> segundo_nombre = $segundo_nombre;
+            $this-> primer_apellido = $primer_apellido;
+            $this-> segundo_apellido = $segundo_apellido;
+            $this-> fecha_nacimiento = $fecha_nacimiento;
+            $this-> foto = $foto;
+            $this-> codigo_credencial = $codigo_credencial;
+        } 
+        
     }
